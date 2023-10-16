e7fed225e16d98b3518ea5bbc6e8bb198d5fa44b
BrianRF86 <brianruiz.flynn@live.com>
2023-10-16 13:49:54 +0100

    so pa PO game elements

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,40 @@
 #include "raylib.h"
 
+
 int main() {
     // Determin the Game Window Width and Height
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 800;
 
     // Initialize the Window
-    InitWindow(screenWidth, screenHeight, "My Game");
+    InitWindow(screenWidth, screenHeight, "So_pa_PO!");
+
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
 
-    // The Game Loop
-    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
 
-        // Setup Canvas
+    // The Game Loop
+while (WindowShouldClose() == false) {
+
+
+  // Setup Canvas
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
-        ClearBackground(RAYWHITE);
-
-        // Here goes all the Game Logic
-
-        // teardown Canvas
+        //Creating game elements paddles, ball, and midcourt line
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
+DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+DrawRectangle(10,screenHeight / 2 - 60,25,120,WHITE);
+DrawRectangle(screenWidth-35, screenHeight / 2 - 60, 25, 120, WHITE);
         EndDrawing();
-    }
+
+}
+
+        
+    
+
+//De-initialization
+
     CloseWindow();
     return 0;
 }
