b92cd1a815bb25e30d7a0034e99df2a582bad362
BrianRF86 <brianruiz.flynn@live.com>
2023-10-16 13:55:05 +0100

    Update ball parameters

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,20 @@
 #include "raylib.h"
 
+//Defining ball parameters
+class Ball{
+public: 
+float x, y;
+int speed_x, speed_y;
+int radius;
+
+void Draw () {
+    DrawCircle(x, y, radius, WHITE);
+}
+
+};
+
+Ball ball;
+
 
 int main() {
     // Determin the Game Window Width and Height
@@ -13,6 +28,12 @@ int main() {
     // Setting the Frames Per Second
     SetTargetFPS(60);
 
+//Setting ball radius
+ball.radius = 20;
+ball.x = screenWidth/2;
+ball.y = screenHeight/2;
+ball.speed_x = 7;
+ball.speed_y = 7;
 
     // The Game Loop
 while (WindowShouldClose() == false) {
@@ -23,7 +44,7 @@ while (WindowShouldClose() == false) {
         // Clear canvas to a specific color to avoid flicker
         //Creating game elements paddles, ball, and midcourt line
         DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE);
-DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE);
+ball.Draw();
 DrawRectangle(10,screenHeight / 2 - 60,25,120,WHITE);
 DrawRectangle(screenWidth-35, screenHeight / 2 - 60, 25, 120, WHITE);
         EndDrawing();
